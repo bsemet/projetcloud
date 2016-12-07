7896f9243c8e224e3dc4036c98363f3fe7e268db
CorentinBiteau <c.biteau@gmail.com>
2016-12-07 16:36:40 +0100

    Récupérer HTML file

--- HttpServer.rb
+++ HttpServer.rb
@@ -1,58 +1,102 @@
 require 'socket' # Provides TCPServer and TCPSocket classes
-
+require 'uri'
 # Initialize a TCPServer object that will listen
 # on localhost:2345 for incoming connections.
 ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
 server = TCPServer.new(ip.ip_address,80)
 
-# loop infinitely, processing one incoming
-# connection at a time.
+
+# Files will be served from this directory
+WEB_ROOT = './'
+
+# Map extensions to their content type
+CONTENT_TYPE_MAPPING = {
+  'html' => 'text/html',
+  'txt' => 'text/plain',
+  'png' => 'image/png',
+  'jpg' => 'image/jpeg'
+}
+
+# Treat as binary data if content type cannot be found
+DEFAULT_CONTENT_TYPE = 'application/octet-stream'
+
+# This helper function parses the extension of the
+# requested file and then looks up its content type.
+
+def content_type(path)
+  ext = File.extname(path).split(".").last
+  CONTENT_TYPE_MAPPING.fetch(ext, DEFAULT_CONTENT_TYPE)
+end
+
+# This helper function parses the Request-Line and
+# generates a path to a file on the server.
+
+def requested_file(request_line)
+  request_uri  = request_line.split(" ")[1]
+  path         = URI.unescape(URI(request_uri).path)
+
+  clean = []
+
+  # Split the path into components
+  parts = path.split("/")
+
+  parts.each do |part|
+    # skip any empty or current directory (".") path components
+    next if part.empty? || part == '.'
+    # If the path component goes up one directory level (".."),
+    # remove the last clean component.
+    # Otherwise, add the component to the Array of clean components
+    part == '..' ? clean.pop : clean << part
+  end
+
+  # return the web root joined to the clean path
+  File.join(WEB_ROOT, *clean)
+end
+
+# Except where noted below, the general approach of
+# handling requests and generating responses is
+# similar to that of the "Hello World" example
+# shown earlier.
+
 loop do
+  socket       = server.accept
+  request_line = socket.gets
 
-  # Wait until a client connects, then return a TCPSocket
-  # that can be used in a similar fashion to other Ruby
-  # I/O objects. (In fact, TCPSocket is a subclass of IO.)
-  socket = server.accept
+  STDERR.puts request_line
 
-  # Read the first line of the request (the Request-Line)
-  request = socket.gets
+  path = requested_file(request_line)
+  path = File.join(path, 'index.html') if File.directory?(path)
 
-  # Log the request to the console for debugging
-  STDERR.puts request
+  # Make sure the file exists and is not a directory
+  # before attempting to open it.
+  if File.exist?(path) && !File.directory?(path)
+    File.open(path, "rb") do |file|
+      socket.print "HTTP/1.1 200 OK\r\n" +
+                   "Content-Type: #{content_type(file)}\r\n" +
+                   "Content-Length: #{file.size}\r\n" +
+                   "Connection: close\r\n"
 
-  response = "<!doctype html>
-    <html>
-    <head>
-      <meta charset='utf-8'>
-      <title>Répertoire des prénoms</title>
-    </head>
-    <body>
-      <h1>Répertoire des prénoms</h1>
-      <p>Entrez votre prénom ici : </p>
-      <form action='todo.js'>
-        <input type='text' name='prenom'>
-      <input type='submit' value='Validez'><br>
-      <p>Projet Cloud par SMAB</p>
-    </body>
-    </html>"
+      socket.print "\r\n"
 
-  # We need to include the Content-Type and Content-Length headers
-  # to let the client know the size and type of data
-  # contained in the response. Note that HTTP is whitespace
-  # sensitive, and expects each header line to end with CRLF (i.e. "\r\n")
-  socket.print "HTTP/1.1 200 OK\r\n" +
-               "Content-Type: text/html\r\n" +
-               "Content-Length: #{response.bytesize}\r\n" +
-               "Connection: close\r\n"
+      # write the contents of the file to the socket
+      IO.copy_stream(file, socket)
+    end
+  else
+    message = "File not found\n"
 
-  # Print a blank line to separate the header from the response body,
-  # as required by the protocol.
-  socket.print "\r\n"
+    # respond with a 404 error code to indicate the file does not exist
+    socket.print "HTTP/1.1 404 Not Found\r\n" +
+                 "Content-Type: text/plain\r\n" +
+                 "Content-Length: #{message.size}\r\n" +
+                 "Connection: close\r\n"
 
-  # Print the actual response body, which is just "Hello World!\n"
-  socket.print response
+    socket.print "\r\n"
 
-  # Close the socket, terminating the connection
+    socket.print message
+  end
+
   socket.close
 end
+
+
 
