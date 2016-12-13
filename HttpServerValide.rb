58dc45063ded172f0f861fc8f9755bc55b110579
Martin Florian <martinflorian0808@gmail.com>
2016-12-13 16:57:50 +0100

    Update HttpServerValide.rb

--- HttpServerValide.rb
+++ HttpServerValide.rb
@@ -5,6 +5,7 @@ require 'redis'
 
 
 db = Redis.new( :host => "localhost", :port => 6379) #Localhost si no docker
+db.save
 db.set("iterator",0)
 # Initialize a TCPServer object that will listen
 # on localhost:2345 for incoming connections.
