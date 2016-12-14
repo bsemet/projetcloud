3a5efce5276e78d3c0167c2a5d1cf7cb212ef9c3
Martin Florian <martinflorian0808@gmail.com>
2016-12-14 12:28:23 +0100

    Update HttpServer.rb

--- HttpServer.rb
+++ HttpServer.rb
@@ -2,7 +2,7 @@ require 'socket' # Provides TCPServer and TCPSocket classes
 require 'uri'
 require 'redis' 
 
-db = Redis.new( :host => "localhost", :port => 6379) #Localhost si no docker
+db = Redis.new( :host => "localhost", :port => 6379, :password=>"password" ) #Localhost si no docker
 db.set("iterator",0)
 # Initialize a TCPServer object that will listen
 # on localhost:2345 for incoming connections.
